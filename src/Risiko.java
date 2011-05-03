3e81a20fb4447578d80eabb5947dee9f17855ad5
Hendrik <HendrikDruse@web.de>
2011-05-03 16:21:19 +0200

    Aktuelle Datei um siehst du im LOG
    Signed-off-by: Hendrik <HendrikDruse@web.de>

--- src/Risiko.java
+++ src/Risiko.java
@@ -1,8 +1,21 @@
 public class Risiko {
 	
+	private int spielerzahl;
+	private Spieler[] spieler;
+	
+	public Risiko() {
+		Laenderverwaltung laenderverwaltung = new Laenderverwaltung();
+		spielerzahl = IO.readInt("Wieviele Spieler?: ");
+		
+		spieler = new Spieler[spielerzahl];
+		
+		for(int i = 0; i < spielerzahl; i++){
+			String name = IO.readString("Name Spieler "+ (i+1) +": ");
+			spieler[i] = new Spieler(name);
+		}
+	}
 	
 	public static void main(String args[]) {
-		Laenderverwaltung laenderverwaltung = new Laenderverwaltung();
-		IO.readInt("Wieviele Spieler?: ");
+		Risiko risiko = new Risiko();		
 	}
 }
