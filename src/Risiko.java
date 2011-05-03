d86f4c0f3a2ea9cc71d01bcc0a41adb1cc1624df
Hendrik <HendrikDruse@web.de>
2011-05-03 16:04:24 +0200

    IO.Klasse von teschke drin

--- src/Risiko.java
+++ src/Risiko.java
@@ -5,5 +5,7 @@ public class Risiko {
 	 * @param
 	 */
 	public static void main(String args[]) {
+		Laenderverwaltung laenderverwaltung = new Laenderverwaltung();
+		IO.readInt("Wieviele Spieler?: ");
 	}
 }
