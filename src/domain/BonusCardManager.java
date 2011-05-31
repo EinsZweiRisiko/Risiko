41458721bc1e5ed834948e1b3979f893fc125bad
Jannes <jannes.meyer@gmail.com>
2011-05-31 17:18:19 +0200

    Added BonusCardManager and BonusCard

---
+++ src/domain/BonusCardManager.java
@@ -0,0 +1,19 @@
+package domain;
+
+import valueobjects.BonusCard;
+
+public class BonusCardManager {
+
+	/**
+	 * Constructor
+	 */
+	public BonusCardManager() {
+		//
+	}
+	
+	public BonusCard retrieveRandomCard() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
