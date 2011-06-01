a2dc9a24d650273b2f20bffb978d5804f2379f82
Jannes <jannes.meyer@gmail.com>
2011-06-01 18:31:00 +0200

    created BonusCard class

--- src/valueobjects/BonusCard.java
+++ src/valueobjects/BonusCard.java
@@ -2,4 +2,14 @@ package valueobjects;
 
 public class BonusCard {
 
+	public static enum Types {
+		Infantry, Cavalry, Artillery
+	};
+	
+	public BonusCard() {
+//		for ()
+		// 14 karten jeweils
+		// 2 joker
+	}
+	
 }
