d87d6b70473d9bf6e1ab45dd5310d9895daebff8
Jannes <jannes.meyer@gmail.com>
2011-06-07 22:53:15 +0200

    BonusCard todo added

--- src/domain/BonusCardManager.java
+++ src/domain/BonusCardManager.java
@@ -8,6 +8,7 @@ public class BonusCardManager {
 	 * Constructor
 	 */
 	public BonusCardManager() {
+		// TODO setup bonus cards at the beginning of a game
 		// 14 von jeder sorte
 		// 2 joker
 	}
--- src/valueobjects/BonusCard.java
+++ src/valueobjects/BonusCard.java
@@ -1,15 +1,22 @@
 package valueobjects;
 
+/**
+ * This class represents a bonus card that a player receives at the end of his turn if he captured at
+ * least one territory.
+ * 
+ * These cards are also referred to as "RISK cards" in the Risk manual.
+ * 
+ * @author Jannes
+ * 
+ */
 public class BonusCard {
 
 	public static enum Types {
-		Infantry, Cavalry, Artillery
+		Infantry, Cavalry, Artillery, WildCard
 	};
-	
+
 	public BonusCard() {
-//		for ()
-		// 14 karten jeweils
-		// 2 joker
+
 	}
-	
+
 }
