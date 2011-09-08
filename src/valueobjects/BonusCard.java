f39177a3b5d00680501466ed7a3c39050a69af48
Jannes <jannes.meyer@gmail.com>
2011-09-09 00:08:21 +0200

    Clean up bonus card logic

--- src/valueobjects/BonusCard.java
+++ src/valueobjects/BonusCard.java
@@ -4,46 +4,35 @@ import java.io.Serializable;
 
 /**
  * This class represents a bonus card that a player receives at the end of his
- * turn if he captured at
- * least one territory.
+ * turn if he captured at least one territory.
  * 
- * These cards are also referred to as "RISK cards" in the Risk manual.
+ * These cards are also referred to as "RISK cards" in the manual.
  * 
  * @author Jannes
  * 
  */
 public class BonusCard implements Serializable {
-	
+
 	private static final long serialVersionUID = -5904440912901516998L;
-	
-//	public static enum Types {
-//		Infantry, Cavalry, Artillery, WildCard
-//	};
-	
+
+	private BonusCardType type;
+
 	/**
-	 * Possible types:
-	 * Infantry, Cavalry, Artillery, WildCard
+	 * Create a bonus card
+	 * 
+	 * @param type
 	 */
-	private String type;
-	
-	public BonusCard(String type) {
-		setType(type);
-	}
-	
-	/**
-	 * Sets the type of a BonusCard
-	 * @param type the type which should be used for the BonusCard
-	 */
-	public void setType(String type) {
+	public BonusCard(BonusCardType type) {
 		this.type = type;
 	}
-	
+
 	/**
+	 * Return the type of the BonusCard
 	 * 
-	 * @return the type of the BonusCard
+	 * @return BonusCardType
 	 */
-	public String getType() {
+	public BonusCardType getType() {
 		return type;
 	}
-	
+
 }
---
+++ src/valueobjects/BonusCardType.java
@@ -0,0 +1,13 @@
+package valueobjects;
+
+/**
+ * The various types of bonus cards.
+ * 
+ * @author Jannes
+ */
+public enum BonusCardType {
+	Infantry,
+	Cavalry,
+	Artillery,
+	Wildcard
+}
