f71446cc9a22bd3f98cfc272b8dcc63ff5eb0b13
Hendrik <HendrikDruse@web.de>
2011-09-12 18:23:37 +0200

    getMyTerritoriesForMoving aufgemöbelt und korrigiert :)

--- src/server/GameMethodsImpl.java
+++ src/server/GameMethodsImpl.java
@@ -564,13 +564,10 @@ public class GameMethodsImpl implements GameMethods, Serializable {
 	@Override
 	public List<Territory> getMyTerritoriesForAttacking(Player player) {
 
-		//TODO das ganze mit Strings versuchen
-
 		List<Territory> territories = getMyTerritories(player);
 		List<Territory> attackingTerritories = new ArrayList<Territory>();
 
 		for(int i = 0; i < territories.size(); i++) {
-			//System.out.println("ES WIRD " + territories.get(i).getName() + " WIRD GEPRÜFT.");
 			List<Territory> neighbors = territories.get(i).getNeighbors();
 			for(int j = 0; j < neighbors.size() ;j++){
 				if(!neighbors.get(j).getOwner().equals(player) && (territoryManager.getTerritoryMap().get(territories.get(i).getName()).getUnitCount() > 1)){
@@ -579,11 +576,6 @@ public class GameMethodsImpl implements GameMethods, Serializable {
 					}
 				}
 			}
-		}
-
-		System.out.println("TERRITORIES FOR ATTACKING: ");
-		for (int i = 0 ; i < attackingTerritories.size(); i++){
-			System.out.println(attackingTerritories.get(i).getName());
 		}
 		return attackingTerritories;
 	}
@@ -595,20 +587,20 @@ public class GameMethodsImpl implements GameMethods, Serializable {
 	 * 
 	 */
 	public List<Territory> getMyTerritoriesForMoving(Player player) {
-		List<Territory> territories = player.getTerritories();
+		
+		List<Territory> territories = getMyTerritories(player);
 		List<Territory> moveTerritories = new ArrayList<Territory>();
 
 		for(int i = 0; i < territories.size(); i++) {
 			List<Territory> neighbors = territories.get(i).getNeighbors();
 			for(int j = 0; j < neighbors.size() ;j++){
-				if(neighbors.get(j).getOwner().equals(player) && territories.get(i).getUnitCount() > 1){
-					if(!moveTerritories.contains(neighbors.get(j))) {
+				if(neighbors.get(j).getOwner().equals(player) && (territoryManager.getTerritoryMap().get(territories.get(i).getName()).getUnitCount() > 1)){
+					if(!moveTerritories.contains(territories.get(i))) {
 						moveTerritories.add(territories.get(i));
 					}
 				}
 			}
 		}
-		System.out.println("Anzahl der zu bewegenen Länder: "+ moveTerritories.size());
 		return moveTerritories;
 	}
 
